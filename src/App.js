9d8327138e45f5405ddee233f9ae01324c2d7a03
Ricky Fan <rickyhzfan@gmail.com>
2021-09-15 12:25:04 -0400

    set up path for bs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Component } from 'react';
 import { Product } from './components/Product';
